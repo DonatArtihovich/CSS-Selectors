ed35d43873ca9105053f0795f532ac60f5967ff2
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2023-06-13 21:38:00 +0300

    refactor: add basic webpack, eslint and typescript settings

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, './rs-css/index.ts'),
+    mode: 'development',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+        assetModuleFilename: pathData => {
+            const filepath = path.dirname(pathData.filename).split('/').slice(2).join('/');
+            return `${filepath}/[name][ext]`;
+        },
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/i, use: 'ts-loader' },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ogg|mp3|wav)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './rs-css/index.html'),
+            filename: 'index.html',
+        }),
+        // new FaviconsWebpackPlugin({
+        //     logo: './migration-to-ts/src/img/icon.png',
+        // }),
+        new EslingPlugin({ extensions: 'ts' })
+    ],
+};
