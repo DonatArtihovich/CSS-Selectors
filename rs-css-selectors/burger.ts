f2ba6cade3c69a5c829f0eeef2db5e9d0dc5163e
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2023-06-15 17:02:05 +0300

    feat: add burger and its animations

---
+++ rs-css-selectors/burger.ts
@@ -0,0 +1,22 @@
+const burger: HTMLDivElement | null = document.querySelector('.burger')
+const overlay: HTMLDivElement | null = document.querySelector('.levels-list__overlay')
+const menu: HTMLDivElement | null = document.querySelector('.levels-list')
+
+enum ActiveClasses {
+  body = 'body_locked',
+  burger = 'burger_active',
+  overlay = 'overlay_active',
+  menu = 'levels-list_active'
+}
+
+function toggleMenu (): void {
+  burger?.classList.toggle(ActiveClasses.burger)
+  menu?.classList.toggle(ActiveClasses.menu)
+  overlay?.classList.toggle(ActiveClasses.overlay)
+  document.body.classList.toggle(ActiveClasses.body)
+}
+
+export default function addBurger (): void {
+  burger?.addEventListener('click', toggleMenu)
+  overlay?.addEventListener('click', toggleMenu)
+}
--- rs-css-selectors/index.ts
+++ rs-css-selectors/index.ts
@@ -2,3 +2,6 @@ import './img/rs_school_js.svg'
 import './img/tick.svg'
 import './img/plate.png'
 import './style.css'
+import addBurger from './burger'
+
+addBurger()
